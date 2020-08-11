998f55796937ff53304063322acaa4c71208eab2
erumaru <toremuratuly.abzal@gmail.com>
2020-08-11 14:45:52 +0500

    Initial Commit

---
+++ stretchy-header/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stretchy-header
+//
+//  Created by Abzal Toremuratuly on 8/11/20.
+//  Copyright © 2020 Abzal Toremuratuly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ stretchy-headerTests/stretchy_headerTests.swift
@@ -0,0 +1,34 @@
+//
+//  stretchy_headerTests.swift
+//  stretchy-headerTests
+//
+//  Created by Abzal Toremuratuly on 8/11/20.
+//  Copyright © 2020 Abzal Toremuratuly. All rights reserved.
+//
+
+import XCTest
+@testable import stretchy_header
+
+class stretchy_headerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
